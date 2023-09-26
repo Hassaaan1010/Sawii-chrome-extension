ddda029973232c0951ac37452fae89a3e9579905
Wajih <hassaaan1010@gmail.com>
2023-09-26 21:36:53 +0530

    storage functionality added

--- Script.js
+++ Script.js
@@ -1,65 +1,59 @@
-// input field element for Title and Url
-titleElement = document.getElementById("title");
-urlElement = document.getElementById("basic-url");
-// Add Link button element
-buttonElement = document.getElementById("button");
+// initializing elements for
+// submit button
+let buttonElement = document.getElementById("button");
+// title input
+let keyElement = document.getElementById("title");
+// url input
+let valueElement = document.getElementById("basic-url");
+// list
+let listElement = document.getElementById("list");
+// complain
+let complainElement = document.getElementById("complain");
 
-// error handling complain element altered for incomplete input or already existing input
-complainElement = document.getElementById("complain");
+// to globalize for future use
+let key, value;
 
-// list element to add new items.
-listElement = document.getElementById("list");
+//allData contains objects representing all key-value pairs in localStorage
+// initializing to itterate through and print out the list
+let allData = Object.entries(localStorage);
 
-// to avoid repetition of tags. on creating a new item
-// Title gets appended/delete to linkList on addToList() and deleteThis()
-let linkList = [];
-
-// to submit the input
-buttonElement.onclick = function () {
-  // onclick => fetches title and url values and stores it in title, url
-  let title = titleElement.value;
-  let url = urlElement.value;
-
-  //to remove error message when proper input is given:
-  complainElement.innerHTML = "";
-
-  //   checks for incomplete input and already existing input
-  if (title != "" && url != "" && linkList.includes(title) === false) {
-    addToList(url, title);
-  } else {
-    complain(url, title);
-  }
-};
-
-// to complain about input
-function complain(aUrl, aTitle) {
-  // for incomplete input
-  if (aTitle === "" || aUrl === "") {
-    complainElement.textContent = "Incomplete input";
-  }
-  // for already existing input
-  else if (linkList.includes(aTitle) === true) {
-    complainElement.textContent = `"${aTitle}" is already a tag. Chose another.`;
-  }
+// prints pre existing list items as list
+for (let each of allData) {
+  printToList(each[0], each[1]);
 }
 
-//to stop complaining when user starts correcting input
-titleElement.onclick = function () {
-  complainElement.innerHTML = "";
-};
-urlElement.onclick = function () {
-  complainElement.innerHTML = "";
+// checks for enter key press
+document.addEventListener("keypress", (event) => {
+  if (event.key == "Enter") {
+    addItem();
+  }
+});
+// checks for submit button
+buttonElement.onclick = function () {
+  addItem();
 };
 
-// to create an item
-function addToList(aUrl, aTitle) {
-  // to add Title to linkList
-  linkList.push(aTitle);
-  localStorage.setItem(aTitle, aUrl);
+// to add item to storage as well as page
+function addItem() {
+  // fetching input
+  key = keyElement.value;
+  value = valueElement.value;
 
-  // to add Title to linkList with a delete button
+  // if input is not repetative and not empty, else it will show a complain
+  if (
+    Object.keys(localStorage).includes(key) === false &&
+    key != "" &&
+    value != ""
+  ) {
+    localStorage.setItem(keyElement.value, valueElement.value);
+    printToList(key, value);
+  } else complain(key, value);
+}
+
+// appends item to to the html list
+function printToList(aTitle, aUrl) {
   listElement.innerHTML += `<div id="${aTitle}" class="list-group-item list-group-item-action">
-  <a class="gourp-item" href="${aUrl}">${aTitle}</a>
+  <a class="group-item" href="${aUrl}" style="font-weight: 700;text-decoration: none;">${aTitle}</a>
   <span class="">
     <svg
       id="i-trash"
@@ -73,7 +67,7 @@ function addToList(aUrl, aTitle) {
       stroke-linecap="round"
       stroke-linejoin="round"
       stroke-width="2"
-      onclick="deleteThis(document.getElementById('${aTitle}'),${aTitle})"
+      onclick="deleteThis(document.getElementById('${aTitle}'),'${aTitle}')"
     >
       <path
         d="M28 6 L6 6 8 30 24 30 26 6 4 6 M16 12 L16 24 M21 12 L20 24 M11 12 L12 24 M12 6 L13 2 19 2 20 6"
@@ -83,10 +77,35 @@ function addToList(aUrl, aTitle) {
   </div>`;
 }
 
+// to complain about input
+function complain(aTitle, aUrl) {
+  // for incomplete input
+  console.log("reached complain");
+  if (aTitle === "" || aUrl === "") {
+    complainElement.textContent = "Incomplete input";
+  }
+  // for already existing input
+  else if (Object.keys(localStorage).includes(aTitle) === true) {
+    console.log("reach ekkada?");
+    complainElement.textContent = `"${aTitle}" is already a tag. Chose another.`;
+  }
+}
+
+//to stop complaining when user starts correcting input
+keyElement.onclick = function () {
+  complainElement.innerHTML = "";
+};
+valueElement.onclick = function () {
+  complainElement.innerHTML = "";
+};
+
 // to delete an item
 function deleteThis(element, aTitle) {
   // removes element form html
   element.remove();
   // removes Title from linkList
-  linkList.splice(linkList.indexOf(aTitle), 1);
+  console.log(element);
+  console.log(aTitle);
+  console.log(typeof aTitle);
+  localStorage.removeItem(aTitle);
 }
